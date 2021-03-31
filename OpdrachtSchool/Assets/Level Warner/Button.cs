5a40d9020a996fb6893ddbef583fe9cdaf9e75ac
JustWarner <78020460+JustWarner@users.noreply.github.com>
2021-03-31 13:55:53 +0200

    Door script/level build
    Door script working en level building noises

---
+++ OpdrachtSchool/Assets/Level Warner/Button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public float afstandRec;
+    public RaycastHit hit;
+    public GameObject buttonImageGo;
+    public Door door;
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, afstandRec))
+        {
+            if(hit.transform.tag == "Button")
+            {
+                buttonImageGo.SetActive(true);
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    print("door open");
+                    //door.doorOpen();
+                }
+            }
+        }
+        else
+        {
+            buttonImageGo.SetActive(false);
+        }
+    }
+}
---
+++ OpdrachtSchool/Assets/Level Warner/Door.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool open;
+    public bool close;
+
+    public void Update()
+    {
+        
+    }
+}
