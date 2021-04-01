80419b9181fe50b0eee06c1c6421b13eb4682e2f
Mart <mart.lammerink@gmail.com>
2021-04-01 15:21:30 +0200

    timer toegevoegd

---
+++ OpdrachtSchool/Assets/Scripts/Teleportal.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleportal : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
